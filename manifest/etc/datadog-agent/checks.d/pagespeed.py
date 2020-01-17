3d5f73279a5ae72ae01953d8d7de1535fa5fee4c
s.nakano <s.nakano@guppy.co.jp>
2020-01-17 11:58:34 +0900

    Update api version 5

--- manifest/etc/datadog-agent/checks.d/pagespeed.py
+++ manifest/etc/datadog-agent/checks.d/pagespeed.py
@@ -17,7 +17,7 @@ class PageSpeedCheck(AgentCheck):
         self.log.info('Running pagespeed check for %s' % (url))
         for strategy in ['desktop', 'mobile']:
             try:
-                api_url = 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=%s&strategy=%s&key=%s' % (url, strategy, google_api_key)
+                api_url = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=%s&strategy=%s&key=%s' % (url, strategy, google_api_key)
 
                 response = requests.get(api_url, timeout=timeout)
                 if response.status_code != 200:
@@ -31,20 +31,30 @@ class PageSpeedCheck(AgentCheck):
                     continue
 
                 loaded_url = pagespeed_result['id']
-                score = pagespeed_result['ruleGroups']['SPEED']['score']
-                median_fcp = pagespeed_result['loadingExperience']['metrics']['FIRST_CONTENTFUL_PAINT_MS']['median']
-                median_dcl = pagespeed_result['loadingExperience']['metrics']['DOM_CONTENT_LOADED_EVENT_FIRED_MS']['median']
+                score = pagespeed_result['lighthouseResult']['categories']['performance']['score'] * 100
+                percentile_fcp = pagespeed_result['loadingExperience']['metrics']['FIRST_CONTENTFUL_PAINT_MS']['percentile']
+                percentile_fid = pagespeed_result['loadingExperience']['metrics']['FIRST_INPUT_DELAY_MS']['percentile']
 
                 metric_tags = ['strategy:' + strategy, 'url:' + loaded_url] + tags
                 self.gauge('pagespeed.score', score, tags=metric_tags, hostname=None, device_name=None)
-                self.gauge('pagespeed.median_fcp', median_fcp, tags=metric_tags, hostname=None, device_name=None)
-                self.gauge('pagespeed.median_dcl', median_dcl, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.percentile_fcp', percentile_fcp, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.percentile_fid', percentile_fid, tags=metric_tags, hostname=None, device_name=None)
 
-                self.log.info('Url: %s strategy: %s page score: %d FCP: %d DCL %d' % (loaded_url, strategy, score, median_fcp, median_dcl))
+                self.log.info('Url: %s strategy: %s page score: %d FCP: %d FID %d' % (loaded_url, strategy, score, percentile_fcp, percentile_fid))
 
-                pageStats = pagespeed_result['pageStats']
-                for metric, value in pageStats.iteritems():
-                    self.gauge('pagespeed.' + metric, value, tags=metric_tags, hostname=None, device_name=None)
+                first_contentful_paint  = pagespeed_result['lighthouseResult']['audits']['first-contentful-paint']['numericValue']
+                speed_index             = pagespeed_result['lighthouseResult']['audits']['speed-index']['numericValue']
+                time_to_interactive     = pagespeed_result['lighthouseResult']['audits']['interactive']['numericValue']
+                first_meaningful_paint  = pagespeed_result['lighthouseResult']['audits']['first-meaningful-paint']['numericValue']
+                first_cpu_idle          = pagespeed_result['lighthouseResult']['audits']['first-cpu-idle']['numericValue']
+                estimated_input_latency = pagespeed_result['lighthouseResult']['audits']['estimated-input-latency']['numericValue']
+
+                self.gauge('pagespeed.first_contentful_paint', first_contentful_paint, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.speed_index', speed_index, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.time_to_interactive', time_to_interactive, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.first_meaningful_paint', first_meaningful_paint, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.first_cpu_idle', first_cpu_idle, tags=metric_tags, hostname=None, device_name=None)
+                self.gauge('pagespeed.estimated_input_latency', estimated_input_latency, tags=metric_tags, hostname=None, device_name=None)
 
             except requests.exceptions.Timeout:
                 self.log.error('Pagespeed API call timed out')
